82cb76252e9893632e247e88b1765856d7dbf982
eailer <elisabeth.ailer@gmx.de>
2023-12-29 10:36:02 +0100

    added weak linear setting

--- src/param_input/linear_weak_n1000_p3_q2.py
+++ src/param_input/linear_weak_n1000_p3_q2.py
@@ -11,24 +11,25 @@ n = 1000
 p = 3
 num_inst = 2
 
-c_X = np.array([1, 1])  # dispersion parameter
-
-# instrument strength
-alpha0 = np.array([4, 1])
-alphaT = np.array([[+0.15, 0.15],
+## Test 3
+c_X = np.array([0.5, 0.5])  # dispersion parameter
+alpha0 = np.array([1, 1])
+alphaT = np.array([[+0.05, 0.01],
                     [0.2, 0]])
-
-# confounder
-mu_c = -2
- # prob of zero inflation
-
-
-# relationship between X and Y
-beta0 = 2
-betaT = np.array([6, 2])
-
-# confounder influence to Y
+mu_c = -3
+beta0 = 5
+betaT = np.array([4, 1])
 c_Y = 4
+
+## Test 2
+#c_X = np.array([1, 1])  # dispersion parameter
+#alpha0 = np.array([4, 1])
+#alphaT = np.array([[+0.15, 0.15],
+#                    [0.2, 0]])
+#mu_c = -2
+#beta0 = 2
+#betaT = np.array([6, 2])#
+#c_Y = 4
 
 
 
